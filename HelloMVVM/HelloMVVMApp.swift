8e9d5b7307438722931f738af7c396e1c3dbca9d
horatio velveteen <antjohn3188@gmail.com>
2026-01-18 09:04:40 -0500

    Initial Commit

---
+++ HelloMVVM/HelloMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMVVMApp.swift
+//  HelloMVVM
+//
+//  Created by Ronald Brown on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloMVVMTests/HelloMVVMTests.swift
@@ -0,0 +1,16 @@
+//
+//  HelloMVVMTests.swift
+//  HelloMVVMTests
+//
+//  Created by Ronald Brown on 1/18/26.
+//
+
+import Testing
+
+struct HelloMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
